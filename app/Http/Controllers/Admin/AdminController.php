d2fd9255ff7e8b7ac354548f207ac1053658b447
744133191@qq.com <744133191@qq.com>
2017-09-11 18:02:53 +0800

    管理员添加/登录功能

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lishuai
+ * Date: 2017/9/11
+ * Time: 15:52
+ */
+
+namespace App\Http\Controllers;
+
+use App\Admininfo;
+use Illuminate\Http\Request;
+use App\User;
+
+class AdminController extends Controller
+{
+    public function addAdmin(Request $request)
+    {
+        $input = $request->all();
+        $validator = Validator::make($input,[
+            'username' => ['required','max:20','regex:/^[a-zA-Z0-9!"#$%&\'()*+,-.\/:;<=>?^_`~{|}\]]+$/','unique:jobs_users,username'],
+            'password' => 'required|min:6|max:60',
+            'passwordConfirm' =>'required|same:password'
+        ]);
+        if ($validator->fails()) {
+            return redirect()->back()->with('error','注册信息格式有误');
+        }
+        $user = new User();
+        $user->username = $input['username'];
+        $user->password = bcrypt($input['password']);
+        $user->type = 3;
+        if($user->save())
+        {
+            $adminInfo = new Admininfo();
+            $adminInfo->uid = $user->uid;
+            $adminInfo->save();
+            return $user;
+        }else{
+            return "管理员添加失败";
+        }
+    }
+    public function deleteAdmin($uid,$aid)
+    {
+        $admin = Admininfo::find($aid);
+        $admin->delete;
+        $user = User::find($uid);
+        $user->delete;
+    }
+}
--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -11,11 +11,41 @@ use App\Http\Controllers\Controller;
 use App\about;
 use Illuminate\Support\Facades\DB;
 use Symfony\Component\Console\Helper\Table;
+use Auth;
+use App\admininfo;
 
 class LoginController extends Controller
 {
-    public function index ()
+    public function __construct() {
+        $this->middleware('guest')->except('logout');  //除了logout方法
+    }
+    public function postLogin(Request $request)
     {
-
+        $input = $request->all();
+        $username = $input['username'];
+        $password = $input['password'];
+        if(Auth::attempt(array('username'=>$username,'password'=>$password)))
+        {
+            $uid = Auth::user()->uid;
+            $type =User::where('uid','=',$uid)
+                ->select(['type'])
+                ->get();
+            $type = $type[0]['type'];
+            session()->put('type',$type);
+            $last_login = date('Y-m-d H-i-s',time());
+            $admin = Admininfo::where('uid',$uid)->get();
+            $admin->last_login = $last_login;
+            $admin->save();
+            return redirect('admin/dashboard');
+        }
+        else{
+            return redirect('admin/login');
+        }
+    }
+    //登出函数
+    public function logout(){
+        Auth::logout();
+        Session::flush();   //清除所有缓存
+        return redirect('index');
     }
 }
