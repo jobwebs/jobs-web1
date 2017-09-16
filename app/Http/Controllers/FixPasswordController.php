cfe96bc77d9ac064658b8b42c78af68115434896
744133191@qq.com <744133191@qq.com>
2017-09-16 22:08:46 +0800

    忘记密码情况下-重置密码

--- app/Http/Controllers/FixPasswordController.php
+++ app/Http/Controllers/FixPasswordController.php
@@ -20,6 +20,7 @@ class FixPasswordController extends Controller
     public function resetPassword(Request $request)
     {
         $data = array();
+        $data['uid'] = AuthController::getUid();
         $input = $request->all();
         $rules = [
             'oldPassword'=>'required|between:6,60',
@@ -30,10 +31,10 @@ class FixPasswordController extends Controller
         if ($validator->fails()) {
             return back();
         }
-        $id = AuthController::getUid();
+        $uid = AuthController::getUid();
         $oldPassword = $request->input('oldPassword');
         $password = $request->input('password');
-        $res = DB::table('jobs_users')->where('uid','=',$id)->select('password')->first();
+        $res = DB::table('jobs_users')->where('uid','=',$uid)->select('password')->first();
         if(!Hash::check($oldPassword, $res->password)){
             $data['status'] = 400;
             $data['msg'] = '原密码不正确';
@@ -42,16 +43,56 @@ class FixPasswordController extends Controller
         $update = array(
             'password' =>bcrypt($password),
         );
-        $result = DB::table('jobs_users')->where('uid',$id)->update($update);
+        $result = DB::table('jobs_users')->where('uid',$uid)->update($update);
         if($result){
             $data['status'] = 200;  //更改密码成功
             $data['msg'] = '密码重置成功';
         }else{
             $data['status'] = 400;  //更改密码失败
-            $data['msg'] = '密码重置成功';
+            $data['msg'] = '密码重置失败';
         }
         $object = new LoginController();
         $object->logout();
         return redirect('account/login');
+    }
+
+//    public function forgotPasswordReset(Request $request)
+//    {
+//        $data = array();
+//        $input = $request->all();
+//        $data['uid'] = $input['uid'];
+//        $rules = [
+//            'password' => 'required|between:6,60',
+//            'passwordConfirm' =>'required|same:password'
+//        ];
+//        $validator = Validator::make($input, $rules);
+//        if ($validator->fails()) {
+//            return back();
+//        }
+//        $password = $request->input('password');
+//        $update = array(
+//            'password' =>bcrypt($password),
+//        );
+//        $result = DB::table('jobs_users')->where('uid',$data['uid'])->update($update);
+//        if($result){
+//            $data['status'] = 200;  //更改密码成功
+//            $data['msg'] = '密码重置成功';
+//        }else{
+//            $data['status'] = 400;  //更改密码失败
+//            $data['msg'] = '密码重置失败';
+//        }
+//        return $data;
+//    }
+    public function forgotPasswordReset($uid,$password)
+    {
+        $update = array(
+            'password' =>bcrypt($password),
+        );
+        $result = DB::table('jobs_users')->where('uid',$uid)->update($update);
+        if($result){
+           return 1;     //密码重置成功
+        }else{
+            return 0;     //密码重置失败
+        }
     }
 }
