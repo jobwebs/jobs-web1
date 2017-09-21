e4cd8f18c6dd5172f30f7c8d3617ee343b18cc27
liuyang <sealiu0217@gmail.om>
2017-09-21 21:19:55 +0800

    完成applyList的页面逻辑

--- resources/views/position/applyList.blade.php
+++ resources/views/position/applyList.blade.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" type="text/css" href="{{asset('plugins/animate-css/animate.min.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset("plugins/sweetalert/sweetalert.css")}}"/>
     <style>
-        .info-card .mdl-card__title small{
+        .info-card .mdl-card__title small {
             font-size: 10px;
             font-weight: 300;
             margin-left: 24px;
@@ -13,12 +13,12 @@
         }
 
         .apply-panel {
-            min-height:500px;
-            padding:0;
+            min-height: 500px;
+            padding: 0;
         }
 
         .apply-item {
-            padding:16px;
+            padding: 16px;
             border-bottom: 1px solid var(--divider);
             cursor: pointer;
             -webkit-transition: all 0.4s ease;
@@ -29,13 +29,13 @@
 
         .apply-item > h5,
         .apply-item > p {
-            margin:0;
+            margin: 0;
             display: inline-block;
-            width:450px;
+            width: 450px;
             font-weight: 300;
         }
 
-        .apply-item>span{
+        .apply-item > span {
             float: right;
             vertical-align: middle;
             text-align: right;
@@ -45,6 +45,12 @@
 
         .apply-item:hover {
             background-color: var(--divider);
+        }
+
+        .mdl-card__title-text {
+            position: relative;
+            top: -3px;
+            margin-left: 16px;
         }
 
     </style>
@@ -69,65 +75,43 @@
             <div class="info-panel">
                 <div class="mdl-card mdl-shadow--2dp base-info--resume info-card">
                     <div class="mdl-card__title">
+                        <button class="mdl-button mdl-button--icon mdl-js-button" id="back-to--message-list"
+                                to="/account/">
+                            <i class="material-icons">arrow_back</i>
+                        </button>
                         <h5 class="mdl-card__title-text">我的申请记录</h5>
                         <small>仅保留近30天的申请记录</small>
                     </div>
 
                     <div class="mdl-card__actions mdl-card--border apply-panel">
+                        @forelse($data["applylist"]["list"] as $position)
+                            <div class="apply-item" data-content="{{$position->fbinfo}}">
+                                <h5>职位名称：{{$position->title}}</h5><br>
+                                <p style="margin-top: 8px;">
+                                    <span>公司名称：{{$data['applylist']['ename'][$position->eid]->ename}}</span>
+                                    <span style="margin-left: 32px;">申请时间：{{$position->created_at}}</span></p>
+                                @if($position->status == 0)
+                                    <span class="normal-info">状态：投递成功</span>
+                                @elseif($position->status == 1)
+                                    <span class="normal-info">状态：企业已查看</span>
+                                @elseif($position->status == 2)
+                                    <span class="success-info">状态：已录用</span>
+                                @elseif($position->status == 3)
+                                    <span class="danger-info">状态：未录用</span>
+                                @elseif($position->status == 4)
+                                    <span class="danger-info">状态：职位已失效</span>
+                                @endif
+                            </div>
+                        @empty
+                            <div class="apply-empty">
+                                <img src="{{asset('images/apply-empty.png')}}" width="50px">
+                                <span>没有申请记录</span>
+                            </div>
+                        @endforelse
 
-                        <div class="apply-item">
-                            <h5>市场专员</h5><br>
-                            <p><span>广州市花都万穗小额贷款股份有限公司</span> | <span>申请时间：08-12</span></p>
-
-                            <span class="normal-info">状态：已查阅</span>
-                        </div>
-
-                        <div class="apply-item">
-                            <h5>市场专员</h5><br>
-                            <p><span>广州市花都万穗小额贷款股份有限公司</span> | <span>申请时间：08-12</span></p>
-
-                            <span class="danger-info">状态：已拒绝</span>
-                        </div>
-
-                        <div class="apply-item">
-                            <h5>市场专员</h5><br>
-                            <p><span>广州市花都万穗小额贷款股份有限公司</span> | <span>申请时间：08-12</span></p>
-
-                            <span class="success-info">状态：已回复</span>
-                        </div>
-
-                        <div class="apply-item">
-                            <h5>市场专员</h5><br>
-                            <p><span>广州市花都万穗小额贷款股份有限公司</span> | <span>申请时间：08-12</span></p>
-
-                            <span class="normal-info">状态：已查阅</span>
-                        </div>
-
-                        <div class="apply-item">
-                            <h5>市场专员</h5><br>
-                            <p><span>广州市花都万穗小额贷款股份有限公司</span> | <span>申请时间：08-12</span></p>
-
-                            <span class="">状态：待查阅</span>
-                        </div>
-
-                        <div class="apply-item">
-                            <h5>市场专员</h5><br>
-                            <p><span>广州市花都万穗小额贷款股份有限公司</span> | <span>申请时间：08-12</span></p>
-
-                            <span class="success-info">状态：已回复</span>
-                        </div>
-
-                        <div class="apply-item">
-                            <h5>市场专员</h5><br>
-                            <p><span>广州市花都万穗小额贷款股份有限公司</span> | <span>申请时间：08-12</span></p>
-
-                            <span class="normal-info">状态：已查阅</span>
-                        </div>
-
-                        <div class="apply-empty">
-                            <img src="{{asset('images/apply-empty.png')}}" width="50px">
-                            <span>没有申请记录</span>
-                        </div>
+                        <nav>
+                            {!! $data["applylist"]["list"]->render() !!}
+                        </nav>
                     </div>
                 </div>
             </div>
@@ -139,9 +123,17 @@
     <script src="{{asset('plugins/sweetalert/sweetalert.min.js')}}"></script>
     <script type="text/javascript">
         $(".apply-item").click(function () {
+
+            var feedback;
+            if ($(this).attr("data-content") === "") {
+                feedback = "暂无回复，当企业回复您的简历时，我们会通过站内信通知您"
+            } else {
+                feedback = $(this).attr("data-content");
+            }
+
             swal({
-                title: "简历回复",
-                text: "简历回复的内容显示在这里：9月21日 据台湾媒体报道今天上午10点，宏达电(HTC)举办重大讯息说明会上，HTC与谷歌共同宣布HTC以11亿美元出售手机业务给谷歌。这一消息已经得到HTC财务长沈道邦的确认。",
+                title: "企业回复",
+                text: feedback,
                 cancelButtonText: "关闭",
                 showCancelButton: true,
                 showConfirmButton: false
