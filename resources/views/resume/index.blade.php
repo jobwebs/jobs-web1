99d2dea11b8b2426fb3b667e45db57e02966b26b
JKJUN0527 <631642753@qq.com>
2017-11-07 16:36:35 +0800

    修改显示简历完成度js名称为小写

--- resources/views/resume/index.blade.php
+++ resources/views/resume/index.blade.php
@@ -171,7 +171,7 @@
 @section('content')
     <div class="info-panel">
         <div class="container" style="height: 30rem;">
-            <h3>即将开放该功能，企业用户可以对简历进行主动筛选查找</h3>
+            <h3>本网站大神库即将开放，即可以根据公司的需求筛选需要的人才</h3>
         </div>
     </div>
 
