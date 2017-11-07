a84b1c696307df56307da2df0b4fb81792b2c350
JKJUN <jkjun0527@foxmail.com>
2017-11-07 19:43:45 +0800

    增加大图15个广告位

--- resources/views/admin/addAds.blade.php
+++ resources/views/admin/addAds.blade.php
@@ -208,7 +208,7 @@
                             <select class="form-control show-tick selectpicker" id="big-image--location"
                                     name="big-image--location">
                                 <option value="0">请选择广告位置</option>
-                                @foreach([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24] as $item)
+                                @foreach([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39] as $item)
                                     <option value="{{$item}}">{{$item}}</option>
                                 @endforeach
 
